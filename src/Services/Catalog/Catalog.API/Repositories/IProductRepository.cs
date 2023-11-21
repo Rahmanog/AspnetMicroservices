4c8505165e5961f77f23cbe3c9e93d1195d1ab14
sitahman0 <44361250+sitahman0@users.noreply.github.com>
2023-11-21 13:52:06 +0700

    Init

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Catalog.API.Model;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<IEnumerable<Product>> GetProductbyName(string name);
+        Task<IEnumerable<Product>> GetProductbyCategory(string categoryName);
+
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+
+        Task<bool> DeleteProduct(string id);
+
+    }
+}
